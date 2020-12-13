9b90b13b3242ef498acd4da9e02e93027651c204
David Mihal <david@dmihal.com>
2020-12-13 11:58:01 +0000

    Add yVault

--- src/singletonAdapters.ts
+++ src/singletonAdapters.ts
@@ -2,24 +2,39 @@ import { Address } from "@graphprotocol/graph-ts"
 import { CompoundAdapter, MappingSet } from "../generated/CompoundAdapter/CompoundAdapter"
 import { Adapter } from "../generated/schema"
 
+let ZERO = Address.fromString('0x0000000000000000000000000000000000000000')
 let COMPOUND_ETH = Address.fromString('0x0000000000000000000000000000000000000001')
 
-function getCompoundAdapter(address: Address): Adapter {
+function getAdapter(address: Address, protocol: String): Adapter {
   let adapter = Adapter.load(address.toHex())
   if (adapter == null) {
     adapter = new Adapter(address.toHex())
-    adapter.protocol = 'Compound'
+    adapter.protocol = protocol
     adapter.supportedWrappers = []
   }
   return adapter!
 }
 
 export function handleCompoundMappingSet(event: MappingSet): void {
-  let adapter = getCompoundAdapter(event.address)
+  let adapter = getAdapter(event.address, 'Compound')
 
   let supportedWrappers = adapter.supportedWrappers
 
   if (event.params.input != COMPOUND_ETH) {
+    supportedWrappers.push(event.params.input.toHex())
+  }
+  supportedWrappers.push(event.params.output.toHex())
+
+  adapter.supportedWrappers = supportedWrappers
+  adapter.save()
+}
+
+export function handleYVaultMappingSet(event: MappingSet): void {
+  let adapter = getAdapter(event.address, 'yEarn')
+
+  let supportedWrappers = adapter.supportedWrappers
+
+  if (event.params.input != ZERO) {
     supportedWrappers.push(event.params.input.toHex())
   }
   supportedWrappers.push(event.params.output.toHex())
